a9302e1ca7985971985d2fb2be6a91a4cced3de3
Michael Aruebo <evanmikeosa@yahoo.com>
2021-04-18 20:43:30 +0100

    second commit

---
+++ Bankly/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Bankly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var sentence = "Michael goes to the stream Michael work at Cyberspace Michael can write C#";
+
+
+            var results = sentence.Split(' ').Where(x => x.Length > 5)
+                              .GroupBy(x => x)
+                              .Select(x => new { Count = x.Count(), Word = x.Key })
+                              .OrderByDescending(x => x.Count);
+
+            foreach (var item in results)
+       
+            Console.WriteLine(String.Format("{0} occured {1} times", item.Word, item.Count));
+
+            Console.ReadLine();
+
+        }
+    }
+}
